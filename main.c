ca7aa2ddd546155630c166b3388153263843c431
Omar Mohamed <es.omar.mostafa2024@alexu.edu.eg>
2025-11-26 07:59:02 +0200

    Made IntialWindow and excluded the build directory

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <raylib.h>
+
+int main(void)
+{
+    InitWindow(1280, 720, "Chess");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
